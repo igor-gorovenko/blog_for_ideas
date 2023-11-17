b9d6f0783318d914afa6d7537895dbd1fe239ec1
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-11-17 15:02:38 +0200

    del welcome and dashboard pages

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,6 @@ use App\Http\Controllers\AdminUserController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -33,11 +25,12 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware(['auth', 'role:user'])->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
     Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
     Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show')->where('id', '[0-9]+');
 });
 
-Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
 
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
 
